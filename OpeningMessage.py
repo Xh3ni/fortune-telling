7d40f02fcea6fcad34c6a6116a00e43aa5819d7d
Xh3ni <xhensilastambolliu@yahoo.com>
2023-01-27 13:19:42 +0000

    Initial commit

---
+++ OpeningMessage.py
@@ -0,0 +1,28 @@
+class OpeningMessage:
+    """
+    The OpeningMessage Class is imported by the main program.
+    When the main program runs
+    a message is displayed. The methods in this class help that
+    message be displayed.
+    """
+    def __init__(self, filename='fortuneTelleingIntroduction.txt'):
+        """
+        the init method defines the filename of the file
+        that will be used.
+        """
+        self.__filename = filename
+
+    def open_file(self):
+        """
+        The open_file method opens the file defined in the init method
+        then returns the contents to variable msg
+        """
+        opening_message = open(self.__filename, "r")
+        msg = opening_message.read()
+        return msg
+
+    def close_file(self):
+        """
+        The close_file method verifies that the file gets closed
+        """
+        open(self.__filename, "r").close()
--- run.py
+++ run.py
@@ -1,3 +1,15 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+This section import the following classes:
+OpeningMessage and EndingMessage
+The random module is also imported
+"""
+from OpeningMessage import OpeningMessage
+"""
+As soon as the program runs a message is displayed.
+This message comes from the OpeningMessage class
+as shown below.
+"""
+m = OpeningMessage()
+m.__init__(filename='fortuneTelleingIntroduction.txt')
+print(m.open_file())
+m.open_file()
